de766812173e2db35c40fe5b1d0651c3ec4b9cf8
kz6fittycent <jimmy.tigert@gmail.com>
2019-04-26 17:18:16 -0500

    Update download-server.py

--- download-server.py
+++ download-server.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
 #THIS SCRIPT DOWNLOADS AND STARTS MINECRAFT SERVER
+#THIS IS INTENDED TO BE USED AS A SNAP BUT CAN BE MODIFIED TO RUN AS A SIMPLE SCRIPT
+
 
 import os.path
 from urllib.request import urlretrieve 
 import subprocess
 import os
 import re
+import time
 
+print (50 * '#')
+print (50 * '-')
+print ("         WELCOME TO MC-SERVER-INSTALLER")
+print (50 * '-')
+print (50 * '#')
+time.sleep(1)
+print ("MIT License")
+time.sleep(1)
+print()
 
 def main():
     
     urlretrieve("https://launcher.mojang.com/v1/objects/f1a0073671057f01aa843443fef34330281333ce/server.jar", "server.jar")
-    subprocess.call(['java','-Xmx4096M', '-Xms1024M','-jar','server.jar','nogui'])
+    subprocess.call(['$SNAP/usr/bin/share/java','-Xmx4096M', '-Xms1024M','-jar','server.jar','nogui'])
     
 main()
