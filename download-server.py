ce7dfe4f731cea3809eaa467cb02bdfaa2016678
kz6fittycent <jimmy.tigert@gmail.com>
2019-04-26 15:37:52 -0500

    Create download-server.py

---
+++ download-server.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#THIS SCRIPT DOWNLOADS AND STARTS MINECRAFT SERVER
+
+import os.path
+from urllib.request import urlretrieve 
+import subprocess
+import os
+import re
+
+
+def main():
+    
+    urlretrieve("https://launcher.mojang.com/v1/objects/f1a0073671057f01aa843443fef34330281333ce/server.jar", "server.jar")
+    subprocess.call(['java','-Xmx4096M', '-Xms1024M','-jar','server.jar','nogui'])
+    
+main()
