f693b3ed661a09ace428a131b41e5c3fe2dc1173
kz6fittycent <jimmy.tigert@gmail.com>
2019-05-03 13:24:11 -0500

    Update download-server.py

--- download-server.py
+++ download-server.py
@@ -8,21 +8,10 @@ from urllib.request import urlretrieve
 import subprocess
 import os
 import re
-import time
-
-print (50 * '#')
-print (50 * '-')
-print ("              MC-SERVER-INSTALLER")
-print (50 * '-')
-print (50 * '#')
-time.sleep(1)
-print ("MIT License")
-time.sleep(1)
-print()
 
 def main():
     
     urlretrieve("https://launcher.mojang.com/v1/objects/f1a0073671057f01aa843443fef34330281333ce/server.jar", "server.jar")
-    subprocess.call(['java','-Xmx4096M', '-Xms1024M','-jar','server.jar','nogui'])
+    #subprocess.call(['java','-Xmx4096M', '-Xms1024M','-jar','server.jar','nogui'])
     
 main()
