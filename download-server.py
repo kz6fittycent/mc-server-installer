558e63b1e4f5e7935dfbd8468670287e35ab7d0d
kz6fittycent <jimmy.tigert@gmail.com>
2019-05-08 17:50:07 -0500

    Update download-server.py

--- download-server.py
+++ download-server.py
@@ -9,9 +9,13 @@ import subprocess
 import os
 import re
 
+# Set path
+os.environ['JAVA_HOME'] = '$SNAP/usr/lib/jvm/java-1.8.0-openjdk-amd64'
+
+# MAIN
 def main():
     
     urlretrieve("https://launcher.mojang.com/v1/objects/f1a0073671057f01aa843443fef34330281333ce/server.jar", "server.jar")
-    #subprocess.call(['java','-Xmx4096M', '-Xms1024M','-jar','server.jar','nogui'])
+    subprocess.call(['JAVA_HOME','-Xmx4096M', '-Xms1024M','-jar','server.jar','nogui'])
     
 main()
