a9d3e0409f4aaf95a99fa3e736b5a44608c116da
kz6fittycent <jimmy.tigert@gmail.com>
2019-05-13 21:28:15 -0500

    Update run-server.sh

--- run-server.sh
+++ run-server.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-# VARIABLES
+# SCRIPT FOR MINECRAFT SERVER 
+
+# Variables
 
 if [ "$SNAP_ARCH" = "amd64" ]; then
   ARCH="x86_64-linux-gnu"
@@ -9,8 +11,6 @@ elif [ "$SNAP_ARCH" = "armhf" ]; then
 else
   ARCH="$SNAP_ARCH-linux-gnu"
 fi
-
-# START THE INSTALLER
 
 echo "Welcome to MC-SERVER-INSTALLER"
 
@@ -21,6 +21,7 @@ export JAVA_HOME=$SNAP/usr/lib/jvm/java-1.8.0-openjdk-$SNAP_ARCH
 export PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH
 
 # Rock n' Roll
+#screen -D
 
 cd $SNAP_USER_DATA
 
@@ -34,55 +35,14 @@ rm server.jar
 
 wget https://launcher.mojang.com/v1/objects/ed76d597a44c5266be2a7fcd77a8270f1f0bc118/server.jar
 
+# Run the server
+
+echo "Running the server..."
+
 sleep 2s 
 
-# Simple menu
+java -Xmx4096M -Xms1024M -jar server.jar nogui
 
-# MENU VARIABLES
+# Agree to the EULA
 
-CHOICE1=$(java -Xmx2048M -Xms1024M -jar server.jar nogui)
-CHOICE2=$(java -Xmx4096M -Xms1024M -jar server.jar nogui)
-CHOICE3=$(java -Xmx6144M -Xms1024M -jar server.jar nogui)
-CHOICE4=$(java -Xmx9192M -Xms1024M -jar server.jar nogui)
-
-while true; do
-
-#DIALOG
-
-echo "\nHow much RAM do you want to allot?\n\n1. 2GB max\n2. 4GB max\n3. 6GB max\n4. 8GB max\n5. Exit\nEnter Selection: "
-
-    read answer
-        case $answer in
-
-        1)    echo Starting server with 2GB max RAM allotted...
-              sleep 1
-              $CHOICE1
-              clear
-              continue;;
-        2)    echo Starting server with 4GB max RAM allotted...
-              sleep 1
-              $CHOICE2
-              clear
-              continue;;
-        3)    echo Starting server with 6GB max RAM allotted...
-              sleep 1
-              $CHOICE3
-              clear
-              continue;;
-        4)    echo Starting server with 8GB max RAM allotted...
-              sleep 1
-              $CHOICE4
-              clear
-              continue;;
-        5)    echo Exiting...
-              sleep 1              
-              # Agree to the EULA
-              echo "Please mark the EULA to eula=true located in ~/snap/mc-installer/current/eula.txt"
-              clear
-              break;;
-        *)    echo Please choose an option between 1-5
-              sleep 1
-              clear
-              continue;;
-    esac
-done
+echo "Please mark the EULA to eula=true located in ~/snap/mc-installer/current/eula.txt"
