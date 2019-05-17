dfe574c962214c78749f27899d9ee7a552c57853
kz6fittycent <jimmy.tigert@gmail.com>
2019-05-16 21:52:46 -0500

    Update server-installer-menu.py

--- server-installer-menu.py
+++ server-installer-menu.py
@@ -5,7 +5,7 @@ import time
 import os
 import sys
 
-os.environ['/snap/mc-server-installer/current']=$SNAP
+os.environ['/snap/mc-server-installer/current']=SNAP
 
 print()
 def print_menu():       
@@ -35,25 +35,25 @@ while loop:
     
     if choice == 1:     
         print ("Starting server with 2GB max...")
-        subprocess.call(['sh','$SNAP/2.sh'])
+        subprocess.call(['sh','SNAP/2.sh'])
     elif choice == 2:
         print ("Starting server with 4GB max...")
-        subprocess.call(['sh','$SNAP/4.sh'])
+        subprocess.call(['sh','SNAP/4.sh'])
     elif choice == 3:
         print ("Starting server with 6GB max...")
-        subprocess.call(['sh','$SNAP/6.sh'])
+        subprocess.call(['sh','SNAP/6.sh'])
     elif choice == 4:
         print ("Starting server with 8GB max...")
-        subprocess.call(['sh','$SNAP/8.sh'])
+        subprocess.call(['sh','SNAP/8.sh'])
     elif choice == 5:
         print ("Starting server with 12GB max...")
-        subprocess.call(['sh','$SNAP/12.sh'])
+        subprocess.call(['sh','SNAP/12.sh'])
     elif choice == 6:
         print ("Starting server with 16GB max...")
-        subprocess.call(['sh','$SNAP/16.sh'])
+        subprocess.call(['sh','SNAP/16.sh'])
     elif choice == 7:
         print ("Starting server with 24GB max...")
-        subprocess.call(['sh','$SNAP/24.sh'])
+        subprocess.call(['sh','SNAP/24.sh'])
     elif choice == 8:
         print ("Exiting. Goodbye!")
         
