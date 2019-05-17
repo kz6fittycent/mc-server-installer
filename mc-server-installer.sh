79ddeee7d357f95f6f642c3a06fc5a149e57d886
kz6fittycent <jimmy.tigert@gmail.com>
2019-05-17 10:29:36 -0500

    Update mc-server-installer.sh

--- mc-server-installer.sh
+++ mc-server-installer.sh
@@ -44,23 +44,35 @@ wget https://launcher.mojang.com/v1/objects/f1a0073671057f01aa843443fef343302813
 sleep 2s 
 
 #DIALOG
+echo "MENU"
+echo ""
+
+sleep 2s
+echo "Please mark the EULA to eula=true located in ~/snap/mc-installer/current/eula.txt"
+echo ""
+
+sleep 4s
 
 options=(
+    "Agree to the EULA"
     "Run MC server with max 2GB of RAM"
     "Run MC server with max 4GB of RAM"
     "Run MC server with max 6GB of RAM"
     "Run MC server with max 8GB of RAM"
+    "Run MC server with max 16GB of RAM"
     "Quit"
 )
 
-PS3="Enter a number (1-5): "
+PS3="Enter a number (1-6): "
 
 select option in "${#options[@]}"; do
     case "$REPLY" in
-        1) java -Xmx2048M -Xmx1024M -jar server.jar nogui;;
-        2) java -Xmx4096M -Xms1024M -jar server.jar nogui;;
-        3) java -Xmx6144M -Xms1024M -jar server.jar nogui;;
-        4) java -Xmx9192M -Xms1024M -jar server.jar nogui;;
-        5) break;;
+        1) sed -ie s/false/true/g eula.txt
+        2) java -Xmx2048M -Xmx1024M -jar server.jar nogui;;
+        3) java -Xmx4096M -Xms1024M -jar server.jar nogui;;
+        4) java -Xmx6144M -Xms1024M -jar server.jar nogui;;
+        5) java -Xmx9192M -Xms1024M -jar server.jar nogui;;
+        6) java -Xmx16384-Xms1024M -jar server.jar nogui;;
+        7) break;;
     esac
 done
